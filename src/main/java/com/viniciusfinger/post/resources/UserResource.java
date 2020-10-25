62ed4882c2f269a4ebb5f4ce68f72afaa58dfc12
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-10-25 18:08:07 -0300

    Tratamento de exceção do USER

--- src/main/java/com/viniciusfinger/post/resources/UserResource.java
+++ src/main/java/com/viniciusfinger/post/resources/UserResource.java
@@ -1,11 +1,13 @@
 package com.viniciusfinger.post.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok(listDto);
-	}	
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok(new UserDTO(user));
+	} 
 }
--- src/main/java/com/viniciusfinger/post/services/UserService.java
+++ src/main/java/com/viniciusfinger/post/services/UserService.java
@@ -1,12 +1,14 @@
 package com.viniciusfinger.post.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciusfinger.post.domain.User;
 import com.viniciusfinger.post.repositories.UserRepository;
+import com.viniciusfinger.post.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,15 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		if (obj.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado no banco.");
+		} else {
+			User user = obj.get();
+			return user; 
+		}	
+	}
 }
+
